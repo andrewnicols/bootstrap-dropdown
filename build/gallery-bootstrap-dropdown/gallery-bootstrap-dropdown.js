7712cb5b7dc10b1dbef9b53c0a4e035b8bf4e827
Andrew Nicols <andrew@nicols.co.uk>
2014-09-17 15:10:49 +0800

    Release version 2014091703

--- build/gallery-bootstrap-dropdown/gallery-bootstrap-dropdown.js
+++ build/gallery-bootstrap-dropdown/gallery-bootstrap-dropdown.js
@@ -1,95 +1,96 @@
 YUI.add('gallery-bootstrap-dropdown', function (Y, NAME) {
 
 /**
-A Plugin which provides dropdown behaviors for dropdown buttons and menu
-groups. This utilizes the markup from the Twitter Bootstrap Project.
-
-@module gallery-bootstrap-dropdown
+ * A Plugin which provides dropdown behaviors for dropdown buttons and menu
+ * groups. This utilizes the markup from the Twitter Bootstrap Project.
+ *
+ * @module gallery-bootstrap-dropdown
 **/
 
 /**
-A Plugin which provides dropdown behaviors for dropdown buttons and menu
-groups. This utilizes the markup from the Twitter Bootstrap Project.
-
-To automatically gain this functionality, you can simply add the
-<code>data-toggle=dropdown</code> attribute to any element.
-
-It can also be plugged into any node or node list.
-
-@example
-
-  var node = Y.one('.someNode');
-  node.plug( Y.Bootstrap.Dropdown );
-  node.dropdown.show();
-
-@class Bootstrap.Dropdown
+ * A Plugin which provides dropdown behaviors for dropdown buttons and menu
+ * groups. This utilizes the markup from the Twitter Bootstrap Project.
+ *
+ * To automatically gain this functionality, you can simply add the
+ * <code>data-toggle=dropdown</code> attribute to any element.
+ *
+ * It can also be plugged into any node or node list.
+ *
+ *     var node = Y.one('.someNode');
+ *     node.plug(Y.Bootstrap.Dropdown);
+ *     node.dropdown.show();
+ *
+ * @namespace Bootstrap
+ * @class Dropdown
 **/
 
 var NS = Y.namespace('Bootstrap');
 
-function DropdownPlugin(config) {
-  DropdownPlugin.superclass.constructor.apply(this, arguments);
+function DropdownPlugin() {
+    DropdownPlugin.superclass.constructor.apply(this, arguments);
 }
 
 DropdownPlugin.NAME = 'Bootstrap.Dropdown';
 DropdownPlugin.NS   = 'dropdown';
 
-Y.extend( DropdownPlugin, Y.Plugin.Base, {
-    defaults : {
-        className : 'open',
-        target    : 'target',
-        selector  : ''
+Y.extend(DropdownPlugin, Y.Plugin.Base, {
+    defaults: {
+        className: 'open',
+        target: 'target',
+        selector: ''
     },
-    initializer : function(config) {
+    initializer: function(config) {
         this._node = config.host;
 
-        this.config = Y.mix( config, this.defaults );
+        this.config = Y.mix(config, this.defaults);
 
-        this.publish('show', { preventable : true, defaultFn : this.show });
-        this.publish('hide', { preventable : true, defaultFn : this.hide });
+        this.publish('show', { preventable: true, defaultFn: this.show });
+        this.publish('hide', { preventable: true, defaultFn: this.hide });
 
         this._node.on('click', this.toggle, this);
     },
 
-    toggle : function() {
+    toggle: function() {
         var target    = this.getTarget(),
             className = this.config.className;
 
-        target.toggleClass( className );
-        target.once('clickoutside', function(e) {
-            target.toggleClass( className );
+        target.toggleClass(className);
+        target.once('clickoutside', function() {
+            target.toggleClass(className);
         });
     },
 
-    show : function() {
-        this.getTarget().addClass( this.config.className );
+    show: function() {
+        this.getTarget().addClass(this.config.className);
     },
-    hide : function() {
-        this.getTarget().removeClass( this.config.className );
+    hide: function() {
+        this.getTarget().removeClass(this.config.className);
     },
-    open : function() {
-        this.getTarget().addClass( this.config.className );
+    open: function() {
+        this.getTarget().addClass(this.config.className);
     },
-    close : function() {
-        this.getTarget().removeClass( this.config.className );
+    close: function() {
+        this.getTarget().removeClass(this.config.className);
     },
 
     /**
-    @method getTarget
-    @description Fetches a Y.NodeList or Y.Node that should be used to modify class names
-    **/ 
-    getTarget : function() {
+     * Fetch a Y.NodeList or Y.Node that should be used to modify class names
+     *
+     * @method getTarget
+     * @return Node|NodeList
+     */
+    getTarget: function() {
         var node     = this._node,
-            selector = node.getData( this.config.target ),
+            selector = node.getData(this.config.target),
             target;
 
-        if ( !selector ) {
+        if (!selector) {
             selector = node.getAttribute('href');
-            selector = target && target.replace(/.*(?=#[^\s]*$)/, ''); //strip for ie7
+            selector = target && target.replace(/.*(?=#[^\s]*$)/, ''); // Strip for ie7.
         }
 
         target = Y.all(selector);
-        if ( target.size() === 0 ) {
+        if (target.size() === 0) {
             target = node.get('parentNode');
         }
 
@@ -104,12 +105,12 @@ NS.dropdown_delegation = function() {
         e.preventDefault();
 
         // Check e.currentTarget, not e.target - prevent strange behaviour when bubbling up.
-        if ( typeof target.dropdown === 'undefined' ) {
-            target.plug( DropdownPlugin );
+        if (typeof target.dropdown === 'undefined') {
+            target.plug(DropdownPlugin);
             target.dropdown.toggle();
         }
-    }, document.body, '*[data-toggle=dropdown]' );
+    }, Y.config.doc.body, '*[data-toggle=dropdown]');
 };
 
 
-}, '2014091702', {"use": ["plugin", "event", "event-outside"]});
+}, '2014091703', {"use": ["plugin", "event", "event-outside"]});
